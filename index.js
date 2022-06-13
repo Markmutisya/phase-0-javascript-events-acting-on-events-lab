3f282b9726c37586df057343a0c2997ad2b0d82b
Markmutisya <mark.mwangi@dtudent.moringaschool.com>
2022-06-13 21:06:33 +0300

    commit tree 5a1968b80751e72603541a395d77a601afe60705

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+const dodger =document.getElementById("dodger");
+const moveDodgerLeft = () => {
+    const leftNumbers =dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left>0){
+        dodger.style.left = `${left -1}px`;
+    }
+}
+const moveDodgerRight = () => {
+    const rightNUmbers = dodger.style.left.replace ("px", "");
+    const right = parseInt(rightNUmbers, 10);
+    if(right<360){
+        dodger.style.left = `${right + 1}px`;
+    }
+}
